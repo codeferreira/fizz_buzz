c29c8f88ec1f4a3aa502c987a740349d87ae47ad
José Ferreira <dev.josejunior@gmail.com>
2023-03-22 20:30:22 -0300

    feat: generate fizzbuzz function

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,25 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
+
+  def handle_file_read({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&convert_to_fizz_buzz/1)
+  end
+
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp convert_to_fizz_buzz(element) do
+    number = String.to_integer(element)
+    evaluate_numbers(number)
+  end
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
